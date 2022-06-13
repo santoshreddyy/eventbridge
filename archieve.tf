04e47287747795cd53915bc97b69fb10c2fa1a9e
awsdevops1234567 <91582898+awsdevops1234567@users.noreply.github.com>
2022-06-13 23:21:44 +0530

    Update archieve.tf

--- archieve.tf
+++ archieve.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudwatch_event_archive" "this" {
   for_each = var.create && var.create_archives ? var.archives : {}
-  name             = each.key
-  event_source_arn = try(each.value["event_source_arn"], aws_cloudwatch_event_bus.this[0].arn)
-  description    = lookup(each.value, "description", null)
+  name             = testarchieve
+  event_source_arn = "arn:aws:events:us-east-1:301388746793:event-bus/default"
+  description    = "test"
   event_pattern  = lookup(each.value, "event_pattern", null)
-  retention_days = lookup(each.value, "retention_days", null)
+  retention_days = "1"
 }
